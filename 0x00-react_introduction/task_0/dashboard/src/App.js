fc5621479a5dc34fcb6abeb01c767e02a089b5c6
kingieX <chimakingsley216@gmail.com>
2023-05-18 16:32:05 +0100

    styling in progress

---
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import holbertonLogo from './holberton_logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
