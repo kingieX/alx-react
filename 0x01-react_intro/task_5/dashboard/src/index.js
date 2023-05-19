e200caf87049aad540280700440efa8b382b2ab6
kingieX <chimakingsley216@gmail.com>
2023-05-19 16:01:56 +0100

    webpack implementation

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Notifications from "./Notifications";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>
+            <Notifications />
+            <App />
+        </div>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
---
+++ 0x01-react_intro/task_5/dashboard/src/setupTests.js
@@ -0,0 +1,4 @@
+import Emzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Emzyme.configure({ adapter: new Adapter() });
