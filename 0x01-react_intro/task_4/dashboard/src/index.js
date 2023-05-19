9e6edc890e11758263629ab7e0477031156bdfb3
kingieX <chimakingsley216@gmail.com>
2023-05-19 12:49:10 +0100

    succesfully ran test

---
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Notifications from "./Notifications";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <div>
+            <Notifications />
+            <App />
+        </div>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
