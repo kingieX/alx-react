bd5230d8ef0feb6628d14f0008c0ecdf17d43d68
kingieX <chimakingsley216@gmail.com>
2023-05-19 15:51:31 +0100

    webpack implementation

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devtool: 'inline-source-map',
+        devServer: {
+            static: './dist',
+            hot: true,
+        },
+        Plugins: [
+            new HtmlWebpackPlugin({
+                template: './dist/index.html',
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    use: ['file-loader'],
+                    options: {
+                        outputPath: 'images',
+                    },
+                },
+            ],
+        },
+    },
+};
