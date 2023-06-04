52984502eb9b0119537bc8d0d123abc2d2bf8921
kingieX <chimakingsley216@gmail.com>
2023-06-05 00:23:19 +0100

    modify app and notificationa and their tests respectively

---
+++ 0x05-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe('<NotificationItem />', () => {
+    it('renders without crashing', () => {
+        shallow(<NotificationItem />);
+    });
+
+    it('renders with correct html when type and value props are provided', () => {
+        const wrapper = shallow(<NotificationItem type='default' value='test' />);
+        expect(wrapper.text()).toEqual('test');
+        expect(wrapper.prop('data-notification-type')).toEqual('default');
+    });
+
+    it('renders with correct html when type and html props are provided', () => {
+        const wrapper = shallow(<NotificationItem type='default' html={{ __html: '<u>test</u>' }} />);
+        expect(wrapper.html()).toContain('<u>test</u>');
+    });
+});
+
+describe("onclick event behaves as it should", () => {
+    it("should call console.log", () => {
+      const wrapper = shallow(<NotificationItem />);
+      const spy = jest.fn();
+  
+      wrapper.setProps({ value: "test item", markAsRead: spy, id: 1 });
+      wrapper.find("li").props().onClick();
+      expect(spy).toBeCalledTimes(1);
+      expect(spy).toBeCalledWith(1);
+      spy.mockRestore();
+    });
+  });
